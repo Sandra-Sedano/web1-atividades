b6211818dd44525124e0cc310f86b15618e0c55f
sandra-sedano <sandrasedano09@gmail.com>
2022-11-02 20:51:02 -0300

    💄 estilizacao

--- src/js/lista-tarefa.js
+++ src/js/lista-tarefa.js
@@ -1,2 +1,17 @@
 import './nav.js'
 import '../css/lista-tarefa.css'
+
+
+document.querySelector('lista-tarefas') 
+
+
+/********************************EVENTO DE CLICK***************************************** */
+btn-adicionar.addEventListener('click',({
+}))
+
+/*************************************CRIAR ELEMENTO**************************************************** */
+document.createElement('tarefa')
+
+
+
+
