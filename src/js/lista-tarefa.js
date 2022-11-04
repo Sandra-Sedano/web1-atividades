fecd4136643853ddba2099239a7f566bcf8ffd4f
sandra-sedano <sandrasedano09@gmail.com>
2022-11-03 21:43:40 -0400

    ✨ funcionalidade: Adicionar tarefa

--- src/js/lista-tarefa.js
+++ src/js/lista-tarefa.js
@@ -1,17 +1,23 @@
 import './nav.js'
 import '../css/lista-tarefa.css'
 
-
-document.querySelector('lista-tarefas') 
-
-
-/********************************EVENTO DE CLICK***************************************** */
-btn-adicionar.addEventListener('click',({
-}))
-
-/*************************************CRIAR ELEMENTO**************************************************** */
-document.createElement('tarefa')
+/************************************* adidionar  ELEMENTO**************************************************** */
 
 
 
+const inputNovaTarefa = document.querySelector('#input-nova-tarefa')
+const btnAdicionar = document.querySelector('#btn-adicionar')
+const listaTarefas = document.querySelector ('#lista-tarefas')
+
+btnAdicionar.addEventListener('click', function(){
+    let descricaoTarefa = inputNovaTarefa.value
+    const li = document.createElement('li')
+li.className = 'tarefa'
+li.innerHTML = `
+<div>${descricaoTarefa}} </div>
+<button class="btn-excluir">Excluir</button>
+`
+listaTarefas.appendChild(li)
+
+})
 
